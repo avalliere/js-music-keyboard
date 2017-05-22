28a9996c5f7186b4b63fc91dc05e5e1bcd851720
Andrea Valliere <amvalliere@gmail.com>
2017-05-22 16:40:43 -0700

    added function to play sounds with key event

--- noise.js
+++ noise.js
@@ -14,4 +14,16 @@ $(document).ready(function() {
     sound.load();
     sound.play();
   });
+
+  $('body').keydown(function(event){
+    // var note = $(this).html();
+    // console.log(note);
+    // var sound = document.getElementById(note + "Audio");
+    // console.log(sound);
+    if (event.key == 'a'|| event.key == 'b' || event.key == 'c' || event.key == 'd' || event.key == 'e' || event.key == 'f' || event.key == 'g') {
+      var sound = document.getElementById(event.key + "Audio");
+      sound.load();
+      sound.play();
+    }
+  });
 });
