e4747e97917fe951c6235825e7eb86fa099e8a61
Andrea Valliere <amvalliere@gmail.com>
2017-05-22 16:01:27 -0700

    jquery function added to play sound on click event

--- noise.js
+++ noise.js
@@ -1,3 +1,17 @@
-$(document).ready( function() {
-  // your code here
+// $(document).ready( function() {
+//   $('.class').click(function(event) {
+//
+//   })
+// });
+
+// plays sound when clicked
+$(document).ready(function() {
+  $('.instrument').on('click', 'button', function(event) {
+    var note = $(this).html();
+    console.log(note);
+    var sound = document.getElementById(note + "Audio");
+    console.log(sound);
+    sound.load();
+    sound.play();
+  });
 });
